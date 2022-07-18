9deddeb6410d8b287f15c164ae2b52203833a6b7
Vishal Pandey <vishalp055@gmail.com>
2022-07-18 17:21:29 +0530

    Complete Project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+
+    public void Reload()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+
+    private int count;
+
+    public TextMeshProUGUI countText;
+    public TextMeshProUGUI winText;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        DisplayText();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement*speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            //Destroy(other.gameObject);
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            DisplayText();
+        }
+    }
+
+    void DisplayText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count == 10)
+        {
+            winText.gameObject.SetActive(true);
+        }
+    }
+}
